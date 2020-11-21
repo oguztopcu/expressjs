fea87b20073c5f3c830f3fb01bbbdc2e9df6d995
Oguz <oguztopcu7@gmail.com>
2020-11-21 21:59:47 +0300

    response type changed html to json

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world!');
+  res.json({
+    message: 'hello world!'
+  });
 });
 
 app.listen(3000, () => {
